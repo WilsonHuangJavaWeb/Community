ed86c475084e54905e54e5ba97b77d0f12e2b520
WilsonHuang <hjn201@gmail.com>
2017-02-21 18:36:23 +0800

    練習

--- src/service/imp/InfoService.java
+++ src/service/imp/InfoService.java
@@ -1,7 +1,32 @@
 package service.imp;
 
+import java.util.List;
+
+import dao.IInfoDAO;
+import dao.imp.InfoDAO;
+import domain.Information;
+import service.IInfoService;
+
 /**
  * Created by ki264 on 2017/2/17.
  */
-public class InfoService {
+public class InfoService implements IInfoService {
+
+    private IInfoDAO infoDAO = new InfoDAO();
+
+    @Override
+    public void addInfo(Information information) {
+        if (information == null)
+            return;
+
+        if (information.getId() != null)
+            return;
+
+        infoDAO.addInfo(information);
+    }
+
+    @Override
+    public List<Information> getAllInfo() {
+        return infoDAO.getAllInfo();
+    }
 }
--- src/service/imp/UserService.java
+++ src/service/imp/UserService.java
@@ -1,5 +1,9 @@
 package service.imp;
 
+import java.util.List;
+
+import dao.IUserDAO;
+import dao.imp.UserDAO;
 import domain.User;
 import service.IUserService;
 
@@ -7,25 +11,87 @@ import service.IUserService;
  * Created by ki264 on 2017/2/17.
  */
 public class UserService implements IUserService {
+    private IUserDAO userDAO = new UserDAO();
 
+    public UserService() {
+    }
 
+    /**
+     * 新增使用者至資料庫，並驗證使用者名稱是否已存在，若存在則部新增
+     *
+     * @param user
+     */
     @Override
     public void addUser(User user) {
 
+        //若使用者為空，則不新增
+        if (user == null)
+            return;
+
+        //若使用者ID不為空，則表示該使用者已存在(無須新增)
+        if (user.getId() != null)
+            return;
+
+        String userName = user.getUserName();
+        List<User> users = userDAO.getUserByUserName(userName);
+
+        //若小於等於0，則表示該使用者名稱已存在
+        if (users.size() <= 0) {
+            userDAO.addUser(user);
+        } else {
+            throw new RuntimeException("該使用者名稱已存在!");
+        }
     }
 
+    /**
+     * 依據ID查詢資料庫中的使用者
+     *
+     * @param id
+     * @return 查詢到的使用者
+     */
     @Override
     public User getUserById(String id) {
-        return null;
+        if (id == null) {
+            return null;
+        }
+
+        return userDAO.getUserById(id);
     }
 
+    /**
+     * 驗證使用者登入的名稱及密碼是否合法
+     *
+     * @param userName 使用者名稱
+     * @param password 登入密碼
+     * @return
+     */
     @Override
     public User validateUser(String userName, String password) {
-        return null;
+        List<User> users = userDAO.getUserByUserName(userName);
+        if (users != null && users.size() != 0) {
+            if (users.get(0).getPassword().equals(password)) {
+                return users.get(0);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
+    /**
+     * 驗證使用者名稱是否存在
+     *
+     * @param userName
+     * @return
+     */
     @Override
     public boolean isUserExist(String userName) {
-        return false;
+        List<User> userList = userDAO.getUserByUserName(userName);
+        if (userList.size() <= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
